fc8a1ac0ec324eef6255969937067c0bd4b0bd8e
Banwind <a84012807@gmail.com>
2023-03-06 00:57:12 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose') // 載入 mongoose
 const bodyParser = require('body-parser')// 引用 body-parser
 const methodOverride = require('method-override')
 const routes = require('./routes') // 引用路由器
-
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-   require('dotenv').config()
-}
-
 const app = express()
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
-const db = mongoose.connection
-
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose') //導入mongoose
 
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -1,18 +1,9 @@
-const mongoose = require('mongoose')
 const restaurants = require('../restaurant_list') // 載入 Restaurant model
 const restaurantsList = require('../../restaurant.json').results
+const db = require('../../config/mongoose')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 db.once('open', () => {
-  console.log('mongodb connected!')
   for (let i=0; i < restaurantsList.length; i++) {
     restaurants.create({ ...restaurantsList[i]})
   }
