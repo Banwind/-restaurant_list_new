d5b56ade3607ff25c6e3aaef83bd3f310f79d89b
Banwind <a84012807@gmail.com>
2023-04-03 16:28:30 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -11,6 +12,33 @@ router.post('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  // 取得表單資料
+  const { name, email, password, confirmPassword } = req.body
+  //檢查是否註冊
+  User.findOne({ email }).then(user => {
+    //如果已經註冊，就返回頁面
+    if (user){
+      console.log('User already register')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      }) 
+    } else {
+        return User.create({
+          name, 
+          email,
+          password
+        })
+          .then(() => res.render('/'))
+          .catch(error => console.log(error))
+    }
+  })
+  .catch(error => console.log(error))
 })
 
 module.exports = router
