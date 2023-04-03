cc7988146ed652ac689ca79518d75a98e3b9ec27
Banwind <a84012807@gmail.com>
2023-04-03 15:32:15 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router() // 準備引入路由模組
 const home = require('./modules/home') // 引入 home 模組程式碼
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
+const users = require('./modules/users')
 
 //    !!!!!!!!!!!!注意先後順序!!!!!!!!!!!!!!
+router.use('/users', users)
 router.use('/restaurants', restaurants)// 將網址結構符合 /restaurants 字串的request 導向 restaurants，裡面的路由清單不再需要處理前綴詞 /restaurants
 router.use('/search', search)
 router.use('/', home)// 將網址結構符合 / 字串的 request 導向 home 模組 
