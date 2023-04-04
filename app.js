f0ce3cd2585009b37102dd6c9dc16a8beac08a09
Banwind <a84012807@gmail.com>
2023-04-04 13:43:04 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')// 引用 body-parser
 const methodOverride = require('method-override')
@@ -9,6 +10,12 @@ require('./config/mongoose') //導入mongoose
 
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false, //當設定為true時，會在每一次與使用者互動時，強制將session更新到session store裡
+  saveUninitialized: true
+}))
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true })) // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
