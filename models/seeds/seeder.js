1a43c0f4b7ee30d90d3478e70f245cc58fbfdff5
Banwind <a84012807@gmail.com>
2023-04-28 11:52:01 +0800

    feat: modify bcrypt seed file

--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -3,19 +3,23 @@ const restaurantsList = require('../../restaurant.json').results
 const db = require('../../config/mongoose')
 const User = require('../user')
 const userList = require('../../user.json').results
+const bcryptUtil = require('../../utils/bcryptUtil')
 
 //利用非同步處理
 db.once('open', async () => {
   try {
+    userList[0].password = await bcryptUtil.hashPassword(userList[0].password)
+    userList[1].password = await bcryptUtil.hashPassword(userList[1].password)
     const user1 = await User.create( {...userList[0]} )
+    user1.password = await bcryptUtil.hashPassword(user1.password)
     for (let i=0; i<3; i++) {
       await restaurants.create({...restaurantsList[i], userId: user1._id})
     }
     const user2 = await User.create({ ...userList[1] })
-    for (let i = 3; i < 6; i++) {
+    for (let i = 3; i<6; i++) {
       await restaurants.create({ ...restaurantsList[i], userId: user2._id })
     }
-    db.close()    
+    db.close()
   } catch (err) {
     console.log(err)
   }
